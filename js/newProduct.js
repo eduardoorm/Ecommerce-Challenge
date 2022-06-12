2d69099842b9ce5c4ff38b52080f7be4ca795b24
eduardo ormeño <eduardoorm79@gmail.com>
2022-06-12 14:15:27 -0500

    added image

--- js/newProduct.js
+++ js/newProduct.js
@@ -1,11 +1,20 @@
+import { validity } from "./validity.js";
+
 const inputFile = document.querySelector(".input-file");
 const textDescriptionProduct = document.querySelector("#descriptionProduct");
+const inputLogin = document.querySelectorAll(".container__forms__input");
+inputLogin.forEach(input=>{
+    input.addEventListener("blur",(input)=>{
+        validity(input.target);
+    })
+})
 
 inputFile.addEventListener("change",(e)=>{
     const containerUrlImg = document.querySelector(".url-file");
     let urlImg = inputFile.files[0].name;
     containerUrlImg.textContent= urlImg;
 })
+
 textDescriptionProduct.addEventListener("blur",(input)=>{
     validity(input.target)
 })
